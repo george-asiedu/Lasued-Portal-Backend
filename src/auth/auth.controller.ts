6fd9f0afdccc92bba8310174340767af93828991
George Asiedu <asiedug41@gmail.com>
2025-02-15 13:06:09 +0000

    controller methods and routes implemented

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,103 @@
+import {
+    Body, Controller, Post,
+    Query, UseGuards, UseInterceptors,
+    UsePipes, ValidationPipe
+} from '@nestjs/common';
+import {
+    ApiBearerAuth, ApiBody, ApiOperation,
+    ApiParam, ApiResponse, ApiTags
+} from '@nestjs/swagger';
+import { TransformInterceptor } from 'src/interceptors/transform.interceptor';
+import {AuthService} from "./auth.service";
+import {RegisterDto} from "./dto/register.dto";
+import {
+    BadRequestExample,
+    LoginBadRequestExample,
+    LoginResponseExample, RegisterResponseExample
+} from 'src/utils/response.model';
+import {VerifyAccountDto} from "./dto/verifyAccount.dto";
+import {SigninDto} from "./dto/signin.dto";
+import { RefreshTokenDto } from './dto/refreshToken.dto';
+import { AuthGuard } from 'src/guards/auth/auth.guard';
+
+@ApiTags('Authentication')
+@Controller('auth')
+@UseInterceptors(TransformInterceptor)
+export class AuthController {
+    constructor(private authService: AuthService) {}
+
+    @Post('register')
+    @UsePipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }))
+    @ApiOperation({ summary: 'Creates a new user into the system.' })
+    @ApiBody({ type: RegisterDto, description: 'JSON structure to create a new user.' })
+    @ApiResponse({
+        status: 201,
+        description: 'Success',
+        example: RegisterResponseExample,
+    })
+    @ApiResponse({
+        status: 400,
+        description: 'Bad Request.',
+        example: BadRequestExample,
+    })
+    async register(@Body() user: RegisterDto) {
+        return await this.authService.register(user);
+    }
+
+    @Post('verify-account')
+    @UsePipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true, }))
+    @ApiOperation({ summary: 'Verifies the 2FA code sent to the user\'s email using token.' })
+    @ApiParam({ name: 'token', description: 'The token for the user to verify their account.' })
+    @ApiBody({ type: VerifyAccountDto, description: '2FA code to verify a user\'s account' })
+    @ApiResponse({
+        status: 200,
+        description: 'Success.',
+        example: { message: 'Success' }
+    })
+    @ApiResponse({
+        status: 400,
+        description: 'Bad Request.',
+        example: { message: 'Invalid 2FA code or user not found.' }
+    })
+    async verifyTwoFactorCode(@Query('token') token: string, @Body() verifyAccountDto: VerifyAccountDto) {
+        return await this.authService.VerifyAccount(verifyAccountDto, token);
+    }
+
+    @Post('signin')
+    @UsePipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true, }))
+    @ApiOperation({ summary: 'Sign in a user into the system.' })
+    @ApiBody({type: SigninDto, description: 'JSON structure to login a user.',})
+    @ApiResponse({
+        status: 200,
+        description: 'Success',
+        example: LoginResponseExample
+    })
+    @ApiResponse({
+        status: 400,
+        description: 'Bad Request',
+        example: LoginBadRequestExample
+    })
+    async signIn(@Body() signInDto: SigninDto) {
+        return this.authService.signIn(signInDto);
+    }
+
+    @ApiBearerAuth()
+    @UseGuards(AuthGuard)
+    @Post('refresh-token')
+    @UsePipes(new ValidationPipe({ forbidNonWhitelisted: true, }))
+    @ApiOperation({ summary: 'Allow continuous user access in the system using a refresh token.' })
+    @ApiBody({ type: RefreshTokenDto, description: 'Refresh token string' })
+    @ApiResponse({
+        status: 200,
+        description: 'Success',
+        example: LoginResponseExample
+    })
+    @ApiResponse({
+        status: 400,
+        description: 'Bad Request.',
+        example: { message: 'Invalid token.' },
+    })
+    async refreshToken(@Body() refreshTokenDto: RefreshTokenDto) {
+        return this.authService.refreshToken(refreshTokenDto);
+    }
+}
---
+++ src/users/users.controller.ts
@@ -0,0 +1,88 @@
+import {Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Req } from '@nestjs/common';
+import {ApiBearerAuth, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import {BaseController} from "../utils/baseController";
+import { UsersService } from './users.service';
+import { Roles } from 'src/guards/roles/roles.decorator';
+import { UserRole } from 'src/model/role.enum';
+import { GetAllUsersResponseExample, UserResponseExample } from 'src/utils/response.model';
+import { User } from 'src/entities/users.entity';
+import { RequestInterface } from 'src/model/auth.model';
+
+@ApiTags('Users')
+@ApiBearerAuth()
+@Controller('users')
+export class UsersController extends BaseController {
+    constructor(private usersService: UsersService) {
+        super();
+    }
+
+    @Get('all-users')
+    @Roles(UserRole.Admin)
+    @ApiOperation({ summary: 'Displays all users in the system.' })
+    @ApiResponse({
+        status: 200,
+        description: 'Success',
+        example: GetAllUsersResponseExample
+    })
+    async getAllUsers(): Promise<User[]> {
+        return await this.usersService.getAllUsers();
+    }
+
+    @Get(':id')
+    @Roles(UserRole.Admin)
+    @ApiOperation({ summary: 'Retrieves a user by ID.' })
+    @ApiResponse({
+        status: 200,
+        description: 'Success',
+        example: UserResponseExample
+    })
+    @ApiResponse({
+        status: 400,
+        description: 'Bad Request.',
+        example: { message: 'Invalid user ID format.' }
+    })
+    async getUserById(@Param('id', ParseIntPipe) id: number): Promise<User | null> {
+        return await this.usersService.getUserById(id);
+    }
+
+    @Get('profile')
+    @ApiOperation({ summary: 'Retrieves the profile of the logged-in user.' })
+    @ApiResponse({
+        status: 200,
+        description: 'Success',
+        example: UserResponseExample
+    })
+    @ApiResponse({
+        status: 400,
+        description: 'Bad Request.',
+        example: { message: 'User ID not valid' }
+    })
+    async getProfile(@Req() req: RequestInterface): Promise<User | null> {
+        return await this.usersService.getUserProfile(req.user);
+    }
+
+    @Patch('update-user')
+    @ApiOperation({ summary: 'Updates the user data info' })
+    @ApiResponse({
+        status: 200,
+        description: 'Success',
+        example: UserResponseExample
+    })
+    @ApiResponse({
+        status: 400,
+        description: 'Bad Request.',
+        example: { message: 'User ID not valid' }
+    })
+    async updateUser(@Param('id') id: number, @Body() updateData: Partial<User>) {
+        return await this.usersService.updateUser(id, updateData);
+    }
+
+    @Delete(':id')
+    @Roles(UserRole.Admin)
+    @ApiOperation({ summary: 'Deletes a user by ID' })
+    @ApiResponse({ status: 200, description: 'Success' })
+    @ApiResponse({ status: 400, description: 'Bad Request', example: { message: 'Invalid user ID format.' } })
+    async deleteUser(@Param('id') id: number) {
+        return this.usersService.deleteUser(id);
+    }
+}
