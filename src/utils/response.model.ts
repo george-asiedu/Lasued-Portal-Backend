6cf80a7f6af4f77e1ad227376fd92e47c47d6e04
George Asiedu <asiedug41@gmail.com>
2025-02-15 11:24:18 +0000

    response object model defined

---
+++ src/utils/response.model.ts
@@ -0,0 +1,54 @@
+export const UserResponseExample = {
+    message: 'Success',
+    data: {
+        user: {
+            id: 1,
+            name: 'George Asiedu',
+            email: 'george.asiedu@gmail.com',
+            role: 'admin',
+            isVerified: false,
+            image: null
+        },
+        token: 'block token'
+    }
+}
+
+export const LoginResponseExample = {
+    message: 'Success',
+    data: {
+        accessToken: 'accessToken',
+        refreshToken: 'refreshToken',
+        user: {
+            email: 'george.asiedu@gmail.com',
+            name: 'George Asiedu',
+            role: 'Admin',
+            isVerified: true,
+            image: null
+        }
+    }
+}
+
+export const GetAllUsersResponseExample = {
+    message: 'Success',
+    data: [
+        {
+            id: 1,
+            name: 'George Asiedu',
+            email: 'george.asiedu@gmail.com',
+            role: 'admin',
+            refreshToken: null,
+            isVerified: true
+        }
+    ]
+}
+
+export const BadRequestExample = [
+    'name should not be empty',
+    'email must be an email',
+    'password must be at least 8 characters long'
+]
+
+export const LoginBadRequestExample = [
+    'email must be an email',
+    'password must be at least 8 characters long'
+]
