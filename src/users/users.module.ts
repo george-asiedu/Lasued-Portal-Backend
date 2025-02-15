eb3a2ec3e2c10e1aca793fefb3196875779a48fc
George Asiedu <asiedug41@gmail.com>
2025-02-15 13:05:40 +0000

    module updated

---
+++ src/users/users.module.ts
@@ -0,0 +1,16 @@
+import { Module } from '@nestjs/common';
+import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from 'src/entities/users.entity';
+import {AuthModule} from "../auth/auth.module";
+
+@Module({
+  imports: [
+    TypeOrmModule.forFeature([User]),
+    AuthModule
+  ],
+  controllers: [UsersController],
+  providers: [UsersService]
+})
+export class UsersModule {}
