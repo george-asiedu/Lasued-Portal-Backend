5723af0b174164ab45d745218b992589ea3491ff
George Asiedu <asiedug41@gmail.com>
2025-02-15 11:24:07 +0000

    constants object defined

---
+++ src/utils/constants.ts
@@ -0,0 +1,11 @@
+export const constants = {
+    swaggerDocsTitle: 'Lasued Student Portal API Documentation',
+    swaggerDocsDescription: 'REST API for managing the Lasued Student Portal',
+    swaggerDocsVersion: '1.0.0',
+    swaggerDocsPath: 'api-docs',
+    productionUrl: 'https://erp-system-hr-module.onrender.com',
+    localUrl: 'http://localhost:',
+    globalPrefix: 'api',
+    ROLES_KEY: 'roles',
+    UnauthorizedError: 'Unauthorized user access',
+}
