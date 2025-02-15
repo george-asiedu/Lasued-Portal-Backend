9eeffcc51d864dfad73d7211b9bb66bc9c8d8bc5
George Asiedu <asiedug41@gmail.com>
2025-02-15 17:04:51 +0000

    C:/Program Files/Git/ removed from production url

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -3,7 +3,7 @@ export const constants = {
     swaggerDocsDescription: 'REST API for managing the Lasued Student Portal',
     swaggerDocsVersion: '1.0.0',
     swaggerDocsPath: 'api-docs',
-    productionUrl: 'https://lasued-student-portal.onrender.com/',
+    productionUrl: 'https://lasued-student-portal.onrender.com',
     localUrl: 'http://localhost:',
     globalPrefix: 'api',
     ROLES_KEY: 'roles',
