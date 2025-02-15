21619cb31eac755450c85d261bf615636b39d862
George Asiedu <asiedug41@gmail.com>
2025-02-15 13:07:41 +0000

    auth guard implemented

---
+++ src/guards/auth/auth.guard.ts
@@ -0,0 +1,42 @@
+import {
+    CanActivate,
+    ExecutionContext,
+    Injectable,
+    UnauthorizedException
+} from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
+import { JwtService } from '@nestjs/jwt';
+import { Request } from "express";
+import { RequestInterface } from "src/model/auth.model";
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+    constructor(
+        private jwtService: JwtService,
+        private configService: ConfigService,
+    ) {}
+
+    async canActivate(context: ExecutionContext): Promise<boolean> {
+        const request = context.switchToHttp().getRequest<RequestInterface>();
+        const token = this.extractTokenFromHeader(request);
+
+        if (!token) {
+            throw new UnauthorizedException();
+        }
+
+        try {
+            request.user = await this.jwtService.verifyAsync(token, {
+                secret: this.configService.get('JWT_SECRET'),
+            });
+        } catch {
+            throw new UnauthorizedException();
+        }
+
+        return true;
+    }
+
+    private extractTokenFromHeader(request: Request): string | undefined {
+        const [type, token] = request.headers.authorization?.split(' ') ?? [];
+        return type === 'Bearer' ? token : undefined;
+    }
+}
