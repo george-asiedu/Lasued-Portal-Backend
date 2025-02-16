0e78dfc3cbcd1911a84f175e9ef2bf30680f198c
George Asiedu <asiedug41@gmail.com>
2025-02-16 17:32:40 +0000

    user and course registration entities added

--- src/courses/courses.module.ts
+++ src/courses/courses.module.ts
@@ -4,10 +4,12 @@ import { CoursesService } from './courses.service';
 import {TypeOrmModule} from "@nestjs/typeorm";
 import {Courses} from "../entities/courses.entity";
 import { AuthModule } from 'src/auth/auth.module';
+import {User} from "../entities/users.entity";
+import { CourseRegistration } from 'src/entities/course-registration.entity';
 
 @Module({
   imports: [
-      TypeOrmModule.forFeature([Courses]),
+      TypeOrmModule.forFeature([Courses, User, CourseRegistration]),
       AuthModule
   ],
   controllers: [CoursesController],
