3bf7a221ddf8fff4150a8e4e3343e6215eece003
George Asiedu <asiedug41@gmail.com>
2025-02-16 17:32:52 +0000

    register courses method implemented

--- src/courses/courses.service.ts
+++ src/courses/courses.service.ts
@@ -1,15 +1,20 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import {BadRequestException, Injectable, NotFoundException} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Courses } from 'src/entities/courses.entity';
 import { Repository } from 'typeorm';
 import { CoursesDto } from './dto/courses.dto';
-
+import {CourseRegistration} from "../entities/course-registration.entity";
+import {User} from "../entities/users.entity";
 
 @Injectable()
 export class CoursesService {
     constructor(
         @InjectRepository(Courses)
         private readonly coursesRepository: Repository<Courses>,
+        @InjectRepository(CourseRegistration)
+        private readonly registrationRepository: Repository<CourseRegistration>,
+        @InjectRepository(User)
+        private readonly userRepository: Repository<User>
     ) {}
 
     async createCourse(courseDto: CoursesDto): Promise<Courses> {
@@ -34,6 +39,28 @@ export class CoursesService {
 
         Object.assign(course, updateData);
         return await this.coursesRepository.save(course);
+    }
+
+    async registerCourse(userId: string, courseId: string): Promise<CourseRegistration> {
+        const student = await this.userRepository.findOne({ where: { id: userId } });
+        if (!student) {
+            throw new NotFoundException(`Student with ID ${userId} not found`);
+        }
+
+        const course = await this.coursesRepository.findOne({ where: { id: courseId } });
+        if (!course) {
+            throw new NotFoundException(`Course with ID ${courseId} not found`);
+        }
+
+        const existingRegistration = await this.registrationRepository.findOne({
+            where: { student, course },
+        });
+        if (existingRegistration) {
+            throw new BadRequestException(`You are already registered for this course.`);
+        }
+
+        const registration = this.registrationRepository.create({ student, course });
+        return await this.registrationRepository.save(registration);
     }
 
     async deleteCourse(id: string): Promise<void> {
